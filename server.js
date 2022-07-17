d76b36e087dc5b56ad075bdae7baac7155cfad9c
Daniel Meyer Dammous <dannydam@gmail.com>
2022-07-17 02:57:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ import fetch from 'node-fetch';
 
 commentsServer();
 
-let bori = {"author":"Testman", "comment":"Another comment"};
-fetch('http://localhost:5000/comments', 
-{
-    body: JSON.stringify(bori),
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'}
-});
+// let bori = {"author":"Testman", "comment":"Another comment"};
+// fetch('http://localhost:5000/comments', 
+// {
+//     body: JSON.stringify(bori),
+//     method: 'POST',
+//     headers: {'Content-Type': 'application/json'}
+// });
 
-fetch('http://localhost:5000/comments/7', 
-{
-    method: 'DELETE',
-});
+// fetch('http://localhost:5000/comments/7', 
+// {
+//     method: 'DELETE',
+// });
