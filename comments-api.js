406a545ef8e4a39522032ef9968403c516a4bc8d
Daniel Meyer Dammous <dannydam@gmail.com>
2022-07-17 04:03:34 -0300

    Update comments-api.js

--- comments-api.js
+++ comments-api.js
@@ -7,6 +7,11 @@ const app = express();
 
 function init(params) {    
     app.use(bodyParser.json({ type: '*/*' }));
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     app.listen(PORT, () => {
      console.log(`Server running on port ${PORT}`);
     });
